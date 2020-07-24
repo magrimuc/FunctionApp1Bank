4c447f8cc057d147388bb04e4dccee1a4fb32a65
Martin Grillo <m.kuemmerer@pharmatechnik.de>
2020-07-24 21:03:26 +0200

    zack

--- Function1.cs
+++ Function1.cs
@@ -32,4 +32,29 @@ namespace FunctionApp1Bank
             return new OkObjectResult(responseMessage);
         }
     }
+    public static class Function2
+    {
+        [FunctionName("Function2")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            //goto DB here
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
 }
+
